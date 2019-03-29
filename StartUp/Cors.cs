678b45d83074373fa3c7d336a0ab2da174ef00c2
morrisgwilliam <morrisgwilliam@gmail.com>
2019-03-29 01:15:02 -0700

    registering application Dependency injection and Authentication/Authorization services

---
+++ StartUp/Cors.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromoARC.StartUp
+{
+    public class Cors
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllCors", builder =>
+                {
+                    builder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .SetIsOriginAllowedToAllowWildcardSubdomains()
+                    .SetIsOriginAllowed(delegate (string requestingOrigin)
+                    {
+                        return true;
+                    }).Build();
+                });
+            });
+
+            //var optionsMVC = new MvcOptions();
+            //I don't think the line above is needed.
+
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowAllCors"));
+            });
+        }
+    }
+}
---
+++ StartUp/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PromoARC.StartUp
+{
+    public class DependencyInjection
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            string connString = configuration.GetConnectionString("Default");
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+    }
+}
